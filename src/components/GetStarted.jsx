1236f77574818782b156ced0f5d5f5329732e692
Enrique Orea <orea_96@hotmail.com>
2022-08-14 12:13:48 -0500

    Get started section finalize

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,14 +4,14 @@ import styles from "../style";
 
 const GetStarted = () => (
   <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
+    className={`group ${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
   >
     <div
-      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
+      className={`${styles.flexCenter} flex-col group-hover:bg-transparent bg-primary w-[100%] h-[100%] rounded-full `}
     >
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
-          <span className="text-gradient">Get</span>
+          <span className=" group-hover:bg-black text-gradient  ">Get</span>
         </p>
         <img
           src={arrowUp}
@@ -20,7 +20,7 @@ const GetStarted = () => (
         />
       </div>
       <p className="font-poppins font-medium text-[18px] leading-[23px]">
-        <span className="text-gradient">Started</span>
+        <span className="text-gradient  ">Started</span>
       </p>
     </div>
   </div>
