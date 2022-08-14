7c5a6c641604456dad249a5e009140c7e24a61be
Enrique Orea <orea_96@hotmail.com>
2022-08-14 12:14:35 -0500

    Create button component for layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles, title }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-lg text-primary outline-none ${styles}`}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
